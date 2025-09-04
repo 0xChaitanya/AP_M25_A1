cdbd7e975f57a7d84a26cdef5cf3447fd1857b2c
0xChaitanya <chaitanyasatsangi26@gmail.com>
2025-09-04 13:09:17 +0530

    abstract class Vehicle

---
+++ Vehicle.java
@@ -0,0 +1,33 @@
+public abstract class Vehicle{
+    private String id;
+    private String model;
+    private double maxSpeed;
+    private double currentMileage;
+
+    public Vehicle(String id, String model, double maxSpeed, double currentMileage){
+        this.id = id;
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.currentMileage = currentMileage;
+    }
+
+    public abstract void move(double distance);
+    public abstract double calculateFuelEfficiency();
+    public abstract double estimateJourneyTime(double distance);
+
+    public void displayInfo(){
+        System.out.println(id);
+        System.out.println(model);
+        System.out.println(maxSpeed);
+        System.out.println(currentMileage);
+    }
+
+    public double getCurrentMileage(){
+        return this.currentMileage;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+}
